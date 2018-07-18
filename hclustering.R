bb3144f0ade9c3b62f2d686cb1cce98f2a97523f
Abby Schmid <34549767+SchmidAbby@users.noreply.github.com>
2018-07-18 11:22:13 -0500

    update
    color labels by actual diagnosis, color branches by predicted diagnosis

--- hclustering.R
+++ hclustering.R
@@ -3,23 +3,27 @@
 
 library(dendextend)
 library(colorspace)
+library(dplyr)
 
 # Data from https://archive.ics.uci.edu/ml/datasets.html
 bdata <- read.csv("Wisconsin_Breast_Cancers.csv", header = TRUE)
 
+# calculating distances and dendrogram 
 bdata_dist <- dist(bdata[,2:10], method = "euclidean")
-
-clust <- hclust(bdata_dist, method = "ward.D", members = NULL)
-
-diagnosis <- unique(bdata[,11])
-
+clust <- hclust(bdata_dist, method = "average", members = NULL)
 dend <- as.dendrogram(clust)
 
+# color based on two groups (prediction of diagnosis)
+group <- c("2","4")
 dend <- color_branches(dend, k=2)
+labels_colors(dend) <- rainbow_hcl(2)[sort_levels_values(group[order.dendrogram(dend)])]
 
-labels_colors(dend) <- rainbow_hcl(2)[sort_levels_values(diagnosis[order.dendrogram(dend)])]
+#adding labels for actual point diagnosis 
+dend2order <- order.dendrogram(dend)
+bdata <- as.tbl(bdata)
+dend2_classes <- pull(bdata[dend2order, 11])
+labels_colors(dend) <- dend2_classes
 
-pdf('breast_cancer_hclust_ward.pdf', width=16, height=10)
+#plotting 
   plot(dend, nodePar = list(cex = .007), main = "Clustering Breast Cancer Data")
-  legend("topleft", legend = diagnosis, fill = rainbow_hcl(2))
-dev.off()
+  legend("topleft", legend = group, fill = rainbow_hcl(2))
